d6f0f3ceba89d24bfc624af9d37d09144cb299c8
Sean Daley <28219661+Scdales@users.noreply.github.com>
2024-06-19 08:06:18 +0100

    refactor: remove redundancies

--- lib/utils/uuid.ts
+++ lib/utils/uuid.ts
@@ -1,11 +1,5 @@
 import { ALPHA_NUMERIC_HYPHEN_REGEX } from '@/lib/constants'
 
-export function isValidUuid(uuid: string) {
-  const splitUuid = uuid.split('-')
-  if (splitUuid.length !== 5) return false
-  return splitUuid.every((segment) => segment.match(ALPHA_NUMERIC_HYPHEN_REGEX)?.length)
-}
-
 export function isValidAlphaNumeric(...args: string[]): boolean {
   return args?.every((arg) => arg.toString().match(ALPHA_NUMERIC_HYPHEN_REGEX)) || false
 }
